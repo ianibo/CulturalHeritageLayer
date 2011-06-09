6c32252165c84cd14f729ee3487fe26ff70d3129
Ian Ibbotson <ianibbo@gmail.com>
2011-06-09 13:10:32 +0100

    Added rest plugin, documented example search from Layar, added sample geo query from europeana

--- webapp/CHLayarService/grails-app/controllers/chlayarservice/GetPOIController.groovy
+++ webapp/CHLayarService/grails-app/controllers/chlayarservice/GetPOIController.groovy
@@ -3,6 +3,21 @@ package chlayarservice
 import grails.converters.*
 
 
+// An example request
+// 6f85d06929d160a7c8a3cc1ab4b54b87db99f74b
+// culturalheritagelayer
+//4.0
+//52.377544
+//4.887339
+//AF
+//en
+//null
+//100
+//null
+//
+// Europeana spatial search http://api.europeana.eu/api/opensearch.rss?searchTerms=enrichment_place_latitude%3A[42+TO+48]+AND+enrichment_place_longitude%3A[10+TO+15]&wskey=xxx
+
+
 class GetPOIController {
 
     def index = { 
